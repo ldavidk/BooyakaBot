f9103cfe811ac56fc6baf86ebe2c30bb61559366
Luke Kelly <luke@getmysa.com>
2019-10-18 15:18:06 -0230

    added things to add in README and (hopefully) privatized the key store

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # BooyakaBot
 Discord Bot themed after Rey Mysterio, for use on RIPTS
+
+Things to add:
+›BETER functionality
+›ASCII art
+›ASCII art randomizer
+›Voice Channel/Streaming capability
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var cron = require('node-cron');
+var fs = require('fs');
 
 client.on('ready', () => {
   console.log('You are now logged in as ${client.user.tag}!');
@@ -9,9 +10,14 @@ client.on('ready', () => {
 // Typing "booyaka booyaka" will make the bot reply with the 619 ascii
 client.on('message', msg => {
   if (msg.content === 'booyaka booyaka') {
-    msg.reply('REAPLCE ME WITH 619 ASCII');
+    msg.reply('6 1 9');
   }
 });
+/*
+so my plan with the above is to eventually be able to randomize which
+version of the art gets sent
+For now/testing ill keep it a simple 6 1 9
+*/
 
 // function to say "BOOYAKA BOOYAKA" in #spam
 function scheduledBooyaka() {
@@ -26,4 +32,5 @@ cron.schedule('19 6 * * *', () => {
 });
 
 // Bot Login
-client.login('TODO Token');
+var key = fs.readFileSync('./private-dc-keys.txt', 'utf8');
+client.login(key);
