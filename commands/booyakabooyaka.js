00deb354ce4df6727df89fa68902e96d78b6d038
ldavidk <lukedavidkelly@gmail.com>
2020-01-09 21:13:57 -0330

    Working bot under new fie scheme

---
+++ commands/booyakabooyaka.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, message) => {
+    return message.channel.send("6 1 9")
+}
+
+module.exports.help = {
+    name: "booyaka booyaka"
+}
--- index.js
+++ index.js
@@ -1,10 +1,59 @@
-const Discord = require("discord.js")
-const config = require("./config.json")
-const token = require("./tokens.json")
+const Discord = require('discord.js')
 
-const bot = Discord.client()
+const fs = require('fs')
 
+const config = require('./config.json')
+const token = require('./tokens.json')
+
+const bot = new Discord.Client()
+
+// Bot Startup
 bot.on('ready', async () => {
     console.log(`${bot.user.username} Online!`)
+    bot.user.setActivity("The 2006 Royal Rumble", {
+        type: "WATCHING"
+    })
 })
 
+// Load Commands
+bot.command = new Discord.Collection()
+fs.readdir('./commands/', (err, files) => {
+    if (err) console.error(err)
+    let commandFiles = files.filter(f => f.split('.').pop() === 'js')
+    if (commandFiles.length <= 0) {
+        console.error('No Commands Found')
+        return
+    }
+    
+    console.log(`Loading ${commandFiles.length} commands...`)
+    commandFiles.forEach((f, index) => {
+        const props = require(`./commands/${f}`)
+        console.log(`${index + 1}: ${f} loaded!`)
+        bot.command.set(props.help.name, props)
+    })
+})
+
+// Message Event
+bot.on("message", async message => {
+    if (message.author.bot) return // If a bot sends the message
+    if (message.channel.type === "dm") return // If its a dm to the bot
+
+    const prefix = config.prefix
+    // let messageArray = message.content.split(" ")
+    // let cmd = messageArray[0]
+    // let args = messageArray.slice(1)
+    const content = message.content.toLowerCase()
+
+    const cmd = bot.command.get(content)
+    if (cmd) {
+        cmd.run(bot, message)
+    }
+
+    // Kill Command (Leave in here as a backup)    
+    if (content === `${prefix}kill`) {
+        console.log('kill command issued')
+        return bot.destroy()
+    }
+})
+
+bot.login(token.token)
